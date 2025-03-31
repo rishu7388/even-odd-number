8b0a12c00afc036b021fd9b1993c6af5ae10bcd0
Rishu <rishuyadavr766@gmail.com>
2025-03-31 22:49:17 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Even Number";
+
+    }
+    else{
+        cout<<"odd number";
+
+    }
+    return 0;
+    
+}
